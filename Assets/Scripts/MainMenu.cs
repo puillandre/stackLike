8e4b2f24f8abefc012f5e0ce28c4ad55f1fdd1c1
Edouard Puillandre <edouard.puillandre@epitech.eu>
2017-11-20 17:14:35 +0100

    add whole game

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public Text scoreText;
+
+	public void Start() {
+		scoreText.text = "HighScore: " + PlayerPrefs.GetInt ("score").ToString();
+	}
+
+	public void ToGame()
+	{
+		SceneManager.LoadScene ("Game");
+	}
+
+	public void ToQuit()
+	{
+		Application.Quit ();
+	}
+}
