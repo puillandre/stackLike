5c148b43eab70ef07b417556e8f0091963a24e2e
Mathieu Sauvau <mathieu.sauvau@gmail.com>
2017-11-24 15:18:43 +0100

    snowflakes particles effect, skybox

---
+++ Assets/Scripts/RandomMaterial.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMaterial : MonoBehaviour {
+
+    public Material[] materials;
+
+	void Start () {
+        GenerateMaterial();
+	}
+
+    public void GenerateMaterial()
+    {
+        int r = Random.Range(0, materials.Length);
+        GetComponent<Renderer>().material = materials[r];
+    }
+}
